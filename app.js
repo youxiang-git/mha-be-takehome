50dc9d89ebb55e7da7080f14aaee2e89d023ad92
youxiang-git <chaiyouxiang@gmail.com>
2024-09-13 12:36:19 +0800

    finish draft one

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const express = require("express");
-const Joi = require("joi");
+const pinoHttp = require("pino-http");
+const pino = require("pino");
 const { v4: uuidv4 } = require("uuid");
+const { schema, idReqSchema } = require("./lib/schema");
+const { StatsService } = require("./lib/StatsService");
 const localData = require("./data.json");
-const { fetchDataOnStartup } = require("./utils");
+const { fetchDataOnStartup } = require("./lib/utils");
+
 const app = express();
-
-const port = 3000;
-let data = {};
-
-const schema = Joi.object({
-	user: Joi.string().alphanum().min(5).max(10).required(),
-	startDateTime: Joi.date().required(),
-	endDateTime: Joi.date()
-		.max("now")
-		.greater(Joi.ref("startDateTime"))
-		.required(),
+const logger = pino({
+	transport: {
+		target: "pino-pretty",
+	},
 });
+const pinoHttpLogger = pinoHttp({ logger });
+const port = 3000;
+const statsService = new StatsService();
+const injectStatsService = (statsService) => (req, res, next) => {
+	req.statsService = statsService;
+	next();
+};
 
-app.use(express.json());
+app.data = [];
+
+// Middleware declared here
+app.use(express.json()); // Use express.json to automatically parse request body to JSON format
+app.use(pinoHttpLogger); // Use pino-http to automatically log all request to our backend
+app.use(injectStatsService(statsService)); // Custom Middleware for Dependency Injection
+
+try {
+	app.data = fetchDataOnStartup(localData);
+	logger.info("Data has been loaded");
+} catch (e) {
+	logger.error("Error loading local data: ", e);
+	return;
+}
 
 app.get("/", (req, res) => {
 	const searchId = req.query.id;
-	if (!searchId) return res.json(data);
+	if (!searchId) return res.json(app.data);
 
-	const entryById = data.find((entry) => entry.id === searchId);
+	const entryById = app.data.find((entry) => entry.id === searchId);
 
 	if (!entryById) {
 		return res
@@ -33,59 +50,63 @@ app.get("/", (req, res) => {
 	return res.json(entryById);
 });
 
-app.get("/:id", (req, res) => {
-	const searchId = req.params.id;
-	const entryById = data.find((entry) => entry.id === searchId);
-
-	if (!entryById) {
+app.get("/:user", (req, res) => {
+	const searchUser = req.params.user;
+	if (!searchUser)
 		return res
-			.status(404)
-			.json({ error: `Entry with ${searchId} not found!` });
+			.status(400)
+			.json({ error: "Please provide a username to filter by!" });
+
+	const userEntries = app.data.filter((entry) => entry.user === searchUser);
+
+	const userEntriesWithStats = req.statsService.generateAllStats(userEntries);
+
+	if (!userEntries || userEntries.length === 0) {
+		return res.status(404).json({ error: `User ${searchUser} not found!` });
 	}
-	return res.json(entryById);
+	return res.json(userEntriesWithStats);
 });
 
 app.post("/", (req, res) => {
 	const { error } = schema.validate(req.body);
 
 	if (error) {
+		logger.error(error);
 		return res.status(400).json({ error: error.message });
 	}
 
-	data = [...data, { id: uuidv4(), ...req.body }];
+	app.data = [...app.data, { id: uuidv4(), ...req.body }];
 
 	return res.json(req.body);
 });
 
 app.delete("/", (req, res) => {
-	const searchId = req.query.id;
+	const { error } = idReqSchema.validate(req.query);
 
-	if (!searchId)
+	if (error) {
+		logger.error(error);
 		return res.status(400).json({
-			error: "Must provide the ID of entry to delete",
+			error: error.message,
 		});
+	}
 
-	const entryToDelete = data.find((entry) => entry.id === searchId);
+	const searchId = req.query.id;
+	const entryToDelete = app.data.find((entry) => entry.id === searchId);
 
 	if (!entryToDelete)
 		return res.status(404).json({ error: "Entry not found!" });
 
-	data = data.filter((entry) => entry.id !== searchId);
+	app.data = app.data.filter((entry) => entry.id !== searchId);
 
 	return res
 		.status(200)
 		.json({ message: "Entry has been successfully deleted!" });
 });
 
-app.listen(port, () => {
-	console.log(`Server running on port: ${port}`);
+if (!module.parent) {
+	app.listen(port, () => {
+		logger.info(`Server running on port: ${port}`);
+	});
+}
 
-	try {
-		data = fetchDataOnStartup(localData);
-		console.log("Data has been loaded");
-	} catch (e) {
-		console.log("Error loading local data");
-		console.log(e);
-		return;
-	}
-});
+module.exports = app;
